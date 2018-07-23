3337497c0681862486f524edfff581c754ac14ca
Tomek Moryl <ramalash@gmail.com>
2018-07-23 09:46:00 +0200

    - You are able to change text in Test-App

--- src/app/index.js
+++ src/app/index.js
@@ -1,17 +1,25 @@
 /**
  * Copyright 2017 by Avid Technology, Inc.
  */
-import { getLocalization } from 'cloudux-l10n';
-import l10nData from '../l10n/lang.en.json';
+
+const {eventEmitter} = require('../eventEmitter/EventEmitter');
+import appConfig from '../package.json';
+
 
 export default class ApplicationContainer {
     constructor() {
+        this.inputString = this.inputString.bind(this);
         this.div = document.createElement('div');
-        this.div.innerHTML = getLocalization(l10nData)('example-plugin-message');
-
+        this.div.innerHTML = `Go to User Settings and use ${appConfig['identity']['appName']} to change this message`;
+        eventEmitter.on('user-tab', (payload) => {
+            this.inputString(payload);
+        });
     }
 
     returnElement() {
         return this.div;
+    }
+    inputString(text) {
+        this.div.innerHTML = text;
     }
 }
--- src/avid_api/settings-tab/UserTabSettings.js
+++ src/avid_api/settings-tab/UserTabSettings.js
@@ -1,8 +1,25 @@
+const {eventEmitter} = require('../../eventEmitter/EventEmitter');
+
+
 export default class UserTabSettings {
     render({domElement}) {
-        let domEdit = document.createElement("p");
-        domEdit.innerHTML = 'Test settings tab !';
+        let domEdit = document.createElement("div");
+        let inputField = document.createElement('input');
+        let button = document.createElement('button');
+        let buttonText = document.createTextNode('Apply');
+        let textNode = document.createTextNode('Enter new message ');
+        inputField.setAttribute('id', 'inputField');
+        button.addEventListener('click', function () {
+            eventEmitter.trigger('user-tab', document.getElementById('inputField').value);
+            console.log('tutaj ' + document.getElementById('inputField').value);
+        });
+        button.appendChild(buttonText);
+        domEdit.appendChild(textNode);
+        domEdit.appendChild(inputField);
+        domEdit.appendChild(button);
         domElement.appendChild(domEdit);
+
         return Promise.resolve(domElement);
     }
 };
+
